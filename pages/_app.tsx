e5596b81c1375368d8d1cceda570b64f1b9b481f
Brennan Fife <brenfife@gmail.com>
2021-03-19 12:31:18 -0500

    more basic setup

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { ChakraProvider } from '@chakra-ui/react';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
-}
