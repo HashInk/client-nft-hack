2535876d5d13dd94165f330cb030c759428d50ff
Brennan Fife <brenfife@gmail.com>
2021-03-19 16:00:36 -0500

    bug fix

--- src/hooks/useInactiveListener.tsx
+++ src/hooks/useInactiveListener.tsx
@@ -3,28 +3,26 @@ import { useEffect } from 'react';
 
 import { injected } from '../utils/connectors';
 
-export default function useInactiveListener(suppress = false) {
+export default function useInactiveListener(suppress: boolean = false) {
   const { active, error, activate } = useWeb3React();
 
-  useEffect(() => {
-    const { ethereum } = window;
-
+  useEffect((): any => {
+    const { ethereum } = window as any;
     if (ethereum && ethereum.on && !active && !error && !suppress) {
       const handleConnect = () => {
         console.log("Handling 'connect' event");
         activate(injected);
       };
-
       const handleChainChanged = (chainId: string | number) => {
         console.log("Handling 'chainChanged' event with payload", chainId);
         activate(injected);
       };
-
       const handleAccountsChanged = (accounts: string[]) => {
         console.log("Handling 'accountsChanged' event with payload", accounts);
-        if (accounts.length > 0) activate(injected);
+        if (accounts.length > 0) {
+          activate(injected);
+        }
       };
-
       const handleNetworkChanged = (networkId: string | number) => {
         console.log("Handling 'networkChanged' event with payload", networkId);
         activate(injected);
