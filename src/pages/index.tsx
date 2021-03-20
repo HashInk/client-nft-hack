9fc273c76f0d33e446c16aa17aba26738744f993
Brennan Fife <brenfife@gmail.com>
2021-03-20 15:34:33 -0500

    adjust

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import { useWeb3React } from '@web3-react/core';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 export default function Redirect(): null {
   const { replace } = useRouter();
+  const { account } = useWeb3React();
+  const JustinsAccount = '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC';
 
   useEffect(() => {
+    // if (account === JustinsAccount) replace('/requests');
+    // else replace('/landing');
     replace('/landing');
-  }, [replace]);
+  }, [replace, account]);
 
   return null;
 }
