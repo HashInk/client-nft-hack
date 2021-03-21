8703f23eae1fde37a9777f9c6b60ca26980b976d
Brennan Fife <brenfife@gmail.com>
2021-03-21 04:04:10 -0500

    -

--- src/components/Dialogs/Sign.tsx
+++ src/components/Dialogs/Sign.tsx
@@ -35,7 +35,7 @@ export default function Sign() {
   const request = {
     albumCover:
       'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=612&h=408&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2016%2F10%2Fharold-berman.jpg',
-    from: 'Miike',
+    from: 'Mikke',
     details: 'Today is my birthday',
   };
   const toast = useToast();
